67923455dd62dfaeb27129e16e50f853e0a4c4c2
MarleneChevalier <chevalierma@hotmail.fr>
2020-05-18 23:12:41 +0200

    Update Projet_ML.html

--- Projet ML_TestR.R
+++ Projet ML_TestR.R
@@ -1,18 +1,53 @@
+# Librairies
+library(caret)
+library(tidyverse)
+library(mice)
+library("RColorBrewer")
+library(VIM)
+library("GGally")
+library("ggmap")
+require(maps)
+library(MASS)
+library(ROCR)
+library(rpart)
+library(rpart.plot)
+library(InformationValue)
 
 
 
 learn = read.csv("projet-app-13-learn.csv",header = TRUE,encoding = "UTF-8")
 test = read.csv("projet-app-13-test.csv",header = TRUE,encoding = "UTF-8")
+summary(learn)
+summary(test)
 View(learn)
 View(test)
-fic.cities = read.csv("cities-gps.csv",header = TRUE,encoding = "UTF-8")
-fic.citiespop = read.csv("cities-population.csv",header = TRUE,encoding = "UTF-8")
-fic.dep = read.csv("departments.csv",header = TRUE,encoding = "UTF-8")
-fic.regions = read.csv("regions.csv",header = TRUE,encoding = "UTF-8")
-View(fic.cities)
-View(fic.dep)
-View(fic.citiespop)
-View(fic.regions)
+
+cities_gps = read.csv("cities-gps.csv",header = TRUE,encoding = "UTF-8")
+cities_population = read.csv("cities-population.csv",header = TRUE,encoding = "UTF-8")
+dept = read.csv("departments.csv",header = TRUE,encoding = "UTF-8")
+regions = read.csv("regions.csv",header = TRUE,encoding = "UTF-8")
+View(cities_gps)
+View(cities_population)
+View(dept)
+View(regions)
+
+# renommage de variable
+learn=learn %>% rename(categorie = catégorie)
+test=test %>% rename(categorie = catégorie)
+
+
+#conversion variables en factor
+learn$reg = as.factor(learn$reg)
+learn$categorie = as.factor(learn$categorie)
+
+test$reg = as.factor(test$reg)
+test$categorie = as.factor(test$categorie)
+
+# retraitement variable cible
+learn$cible= as.character(learn$cible)
+learn$cible[learn$cible == "failure"]=FALSE
+learn$cible[learn$cible == "success"]=TRUE
+learn$cible <- as.logical(learn$cible)
 
 
 learn=merge(x=learn,y=cities_gps,by.x = "insee.code",by.y="id")
